a1770ee1d521d27ced3009f4aab009af044eed17
Sergey Litvinov <litvinows9@gmail.com>
2022-06-27 23:15:52 +0300

    add login with server

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,32 @@
 import React from "react";
 
-function Login() {
+function Login({onLogin}) {
+
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  function handleEmailChange(evt) {
+    setEmail(evt.target.value);
+    // console.log(email);
+  }
+
+  function handlePasswordChange(evt) {
+    setPassword(evt.target.value);
+    // console.log(password);
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    console.log("email: ", email, "pass:", password)
+    // console.log(onRegister);
+    onLogin(email, password); 
+  }
   return (
     <section className="register">
       <h2 className="register__title">Вход</h2>
-      <form className="register__form">
-        <input className="register__input" placeholder="Email"></input>
-        <input className="register__input" placeholder="Пароль"></input>
+      <form className="register__form" onSubmit={handleSubmit}>
+        <input className="register__input" onChange={handleEmailChange} placeholder="Email"></input>
+        <input className="register__input" onChange={handlePasswordChange} placeholder="Пароль"></input>
         <button className="register__submit">Войти</button>
       </form>
     </section>
