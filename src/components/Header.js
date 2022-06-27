657d2588536d88d8e76786908396b368705e67b9
Sergey Litvinov <litvinows9@gmail.com>
2022-06-27 23:31:05 +0300

    created changing links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Link, useRouteMatch } from "react-router-dom";
+import { useLocation } from 'react-router';
 import headerLogo from "../images/logo-mesto.svg";
 import Register from "./Register";
 import Login from "./Login";
 
-function Header() {
+function Header({logoutProfile}) {
 
+  const location = useLocation();
   // const { path, url } = useRouteMatch();
   // console.log('path ', path);
   // console.log('url ', url);
@@ -13,22 +15,45 @@ function Header() {
 
   return (
     <>
-    <header className="header">
-      <Link className="menu__item" to="/">
-        <img className="header__logo" src={headerLogo} alt="лого" />
-      </Link>
+      <header className="header">
+        <Link className="menu__item" to="/">
+          <img className="header__logo" src={headerLogo} alt="лого" />
+        </Link>
 
-      <div className="menu">
-        <Link className="menu__item" to="/sign-in">
-          Login
-        </Link>
-        <Link className="menu__item" to="/sign-up">
-          Register
-        </Link>
-      </div>
-      
-    </header>
-    {/* <Route exact path="/sign-up">
+        <div className="menu">
+
+          <Link to={
+            location.pathname === "/sign-up"
+              ? "/sign-in"
+              : location.pathname === "/sign-in"
+                ? "/sign-up"
+                : "/sign-in"
+          }
+            className="header__link header__link_exit"
+            onClick={location.pathname === "/" ? logoutProfile : () => { }}
+          >
+            {
+              location.pathname === "/sign-up"
+                ? "Войти"
+                : location.pathname === "/sign-in"
+                  ? "Регистрация"
+                  : "Выйти"
+            }
+          </Link>
+
+
+
+
+          {/* <Link className="menu__item" to="/sign-in">
+            Login
+          </Link>
+          <Link className="menu__item" to="/sign-up">
+            Register
+          </Link> */}
+        </div>
+
+      </header>
+      {/* <Route exact path="/sign-up">
       <Register />
     </Route>
     <Route exact path="/sign-in">
