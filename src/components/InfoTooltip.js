b6fe0e6bb377300afda2b1d61235647c59b91616
Sergey Litvinov <litvinows9@gmail.com>
2022-06-27 23:15:17 +0300

    add props popup (open and close)

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -2,15 +2,15 @@ import React from "react";
 import closeIcon from "../images/Close-Icon.svg";
 import successfully from "../images/successfully.svg"
 
-function InfoTooltip() {
+function InfoTooltip(props) {
   return (
-    <section className={`popup popup_opened`}>
+    <section className={`popup ${props.isOpen && "popup_opened"}`}>
       <div className="popup__container">
-        <button id="popup-user-close" type="button" className="popup__button-close">
+        <button id="popup-user-close" type="button" onClick={props.onClose} className="popup__button-close">
           <img className="popup__button-close-img" src={closeIcon} alt="Закрыть"/>
         </button>
         <img className="success" src={successfully} alt="успешно"/>
-        <h className="success-text">Вы успешно зарегистрировались!</h> 
+        <p className="success-text">Вы успешно зарегистрировались!</p> 
       </div>
       <div id="overlayProfile" className="popup__overlay"></div>
     </section>
