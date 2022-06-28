2111ddd95cf9601f5b7c142a16122e671e391f33
Sergey Litvinov <litvinows9@gmail.com>
2022-06-28 22:53:16 +0300

    removed the extra imports

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Link, useRouteMatch } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useLocation } from 'react-router';
 import headerLogo from "../images/logo-mesto.svg";
 import Register from "./Register";
