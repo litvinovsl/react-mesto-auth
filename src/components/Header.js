53cbc5f258888fa900201c908d271284f578a4ff
Sergey Litvinov <litvinows9@gmail.com>
2022-06-29 22:55:20 +0300

    deleted the excess

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,6 @@ import headerLogo from "../images/logo-mesto.svg";
 
 function Header({ logoutProfile, userEmail }) {
 
-  const location = useLocation();
-
   return (
     <header className="header">
       <img className="header__logo" src={headerLogo} alt="лого" />
